7c093faf0b7d6d2bec5639d3fb62d37b790f0aee
Mohsen Taheri <100472003+phantom-bullets@users.noreply.github.com>
2022-10-17 21:27:10 +0330

    1000

---
+++ project 479A Expression.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, max{};
+    cin >> a >> b >> c;
+	if (a+b+c>max)
+		max = a + b + c;
+	if (a*(b+c)>max)
+		max = a * (b + c);
+	if ((a+b)*c>max)
+		max = (a + b) * c;
+	if (a*b*c>max)
+		max = a * b * c;
+	cout << max;
+}
